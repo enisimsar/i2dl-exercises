0bb808f0096ae7ac0454ba8296896d1fcc8c9e7c
Enis Simsar <enisimsar@gmail.com>
2020-03-22 22:04:53 +0300

    last commit

--- exercise_3/exercise_code/rnn/rnn_nn.py
+++ exercise_3/exercise_code/rnn/rnn_nn.py
@@ -70,27 +70,28 @@ class LSTM(nn.Module):
         # nn.Linear() function as your linear layers.                         #
         # Initialse h and c as 0 if these values are not given.               #
         #######################################################################
-
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        
         self.input_size = input_size
         self.hidden_size = hidden_size
 
-        self.input_gate_fc1 = nn.Linear(self.input_size, self.hidden_size)
-        self.input_gate_fc2 = nn.Linear(self.hidden_size, self.hidden_size)
-        self.input_gate_act = nn.Sigmoid()
+        self.input_gate_fc1 = nn.Linear(self.input_size, self.hidden_size).to(device)
+        self.input_gate_fc2 = nn.Linear(self.hidden_size, self.hidden_size).to(device)
+        self.input_gate_act = nn.Sigmoid().to(device)
 
-        self.forgot_gate_fc1 = nn.Linear(self.input_size, self.hidden_size)
-        self.forgot_gate_fc2 = nn.Linear(self.hidden_size, self.hidden_size)
-        self.forgot_gate_act = nn.Sigmoid()
+        self.forgot_gate_fc1 = nn.Linear(self.input_size, self.hidden_size).to(device)
+        self.forgot_gate_fc2 = nn.Linear(self.hidden_size, self.hidden_size).to(device)
+        self.forgot_gate_act = nn.Sigmoid().to(device)
 
-        self.output_gate_fc1 = nn.Linear(self.input_size, self.hidden_size)
-        self.output_gate_fc2 = nn.Linear(self.hidden_size, self.hidden_size)
-        self.output_gate_act = nn.Sigmoid()
+        self.output_gate_fc1 = nn.Linear(self.input_size, self.hidden_size).to(device)
+        self.output_gate_fc2 = nn.Linear(self.hidden_size, self.hidden_size).to(device)
+        self.output_gate_act = nn.Sigmoid().to(device)
 
-        self.memory_cell_fc1 = nn.Linear(self.input_size, self.hidden_size)
-        self.memory_cell_fc2 = nn.Linear(self.hidden_size, self.hidden_size)
-        self.memory_cell_act = nn.Tanh()
+        self.memory_cell_fc1 = nn.Linear(self.input_size, self.hidden_size).to(device)
+        self.memory_cell_fc2 = nn.Linear(self.hidden_size, self.hidden_size).to(device)
+        self.memory_cell_act = nn.Tanh().to(device)
 
-        self.c_act = nn.Tanh()
+        self.c_act = nn.Tanh().to(device)
 
         #######################################################################
         #                           END OF YOUR CODE                          #
@@ -113,17 +114,18 @@ class LSTM(nn.Module):
         #######################################################################
         #                                YOUR CODE                            #
         #######################################################################
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
         (seq_len, batch_size, _) = x.shape
 
         if h is None:
-            h = torch.zeros((1, batch_size, self.hidden_size))
+            h = torch.zeros((1, batch_size, self.hidden_size)).to(device)
         
         if c is None:
-            c = torch.zeros((1, batch_size, self.hidden_size))
+            c = torch.zeros((1, batch_size, self.hidden_size)).to(device)
 
-        h_seq = torch.zeros((seq_len, batch_size, self.hidden_size))
-        c_seq = torch.zeros((seq_len, batch_size, self.hidden_size))
+        h_seq = torch.zeros((seq_len, batch_size, self.hidden_size)).to(device)
+        c_seq = torch.zeros((seq_len, batch_size, self.hidden_size)).to(device)
 
         for i in range(seq_len):
             f_t = self.forgot_gate_act(self.forgot_gate_fc1(x[i]) + self.forgot_gate_fc2(h))
@@ -155,7 +157,7 @@ class RNN_Classifier(torch.nn.Module):
 
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.rnn = nn.RNN(input_size,hidden_size)
+        self.rnn = RNN(input_size,hidden_size)
         self.fc = nn.Linear(hidden_size, classes)
     
     def forward(self, x):
@@ -187,7 +189,7 @@ class LSTM_Classifier(torch.nn.Module):
         #######################################################################
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.lstm = nn.LSTM(input_size,hidden_size)
+        self.lstm = LSTM(input_size, hidden_size)
         self.fc = nn.Linear(hidden_size, classes)
         
     def forward(self, x):
